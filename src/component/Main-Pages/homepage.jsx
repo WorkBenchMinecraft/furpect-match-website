6a7465dd60b390fc09737e6336c36fb70f0dcc0e
Leafar2698 <162131069+WorkBenchMinecraft@users.noreply.github.com>
2026-06-26 18:20:16 +0800

    Homepage Done

--- src/component/Main-Pages/homepage.jsx
+++ src/component/Main-Pages/homepage.jsx
@@ -33,21 +33,21 @@ function Homepage() {
                 </div>
                 <div className="items-center justify-center flex flex-col w-1/2 p-1 gap-3">
                     <img src={mockup} width="450" className="rounded-3xl shadow-lg/20 shadow-taupe-500/50" alt="Mock Up" />
-                    <div className="items-center justify-center w-130 flex gap-1 rounded-3xl p-2 pl-6 border-2 border-gray-50 bg-gray-50">
-                        <div className="justify-center flex gap-2 w-1/3">
-                            <span className="fi fi-sr-paw"></span>
+                    <div className="items-center justify-center w-150 flex gap-1 rounded-3xl p-2 pl-6 border-2 border-gray-50 bg-gray-50">
+                        <div className="justify-center items-center flex gap-2 w-1/3">
+                            <span className="fi fi-sr-heart p-2 size-10 text-xl rounded-full bg-violet-100 text-purple-500"></span>
                             <div className="justify-center flex flex-col w-30 text-left">
                                 <h3>16K+</h3><p>Pets Adopted</p>
                             </div>
                         </div>
-                        <div className="justify-center flex gap-2 w-1/3">
-                            <span className="fi fi-sr-paw"></span>
+                        <div className="justify-center items-center flex gap-2 w-1/3">
+                            <span className="fi fi-sr-shield-check pt-2 text-4xl rounded-full text-lime-500"></span>
                             <div className="justify-center flex flex-col w-40 text-left">
                                 <h3>2.5K+</h3><p className="w-50">Verified Providers</p>
                             </div>
                         </div>
-                        <div className="justify-center flex gap-2 w-1/3">
-                            <span className="fi fi-sr-paw"></span>
+                        <div className="justify-center items-center flex gap-2 w-1/3">
+                            <span className="fi fi-sr-users p-2 size-10 text-xl rounded-full bg-amber-400 text-white"></span>
                             <div className="justify-center flex flex-col w-30 text-left">
                                 <h3>18K+</h3><p>Happy Families</p>
                             </div>
@@ -60,31 +60,31 @@ function Homepage() {
                     <h2 className="font-bold text-xl">Why Choose Furpect Match?</h2>
                     <div className="items-center justify-center w-300 flex gap-10 p-2 px-5">
                         <div className="justify-center items-center flex w-90 text-left gap-3">
-                            <span className="fi fi-sr-sparkles p-4 rounded-full bg-purple-900 size-16 text-white text-3xl"></span>
+                            <span className="fi fi-sr-sparkles p-4 rounded-full bg-violet-300 size-16 text-white text-3xl"></span>
                             <div className="justify-center flex flex-col w-40 text-left">
                                 <h5 className="font-bold text-xl">Smart Watching</h5>
                                 <p className="w-50">AI-Powered Matching to find the perfect pet for you.</p>
                             </div>
                         </div>
                         <div className="justify-center items-center flex w-90 text-left gap-3">
-                            <span className="fi fi-sr-sparkles p-4 rounded-full bg-purple-900 size-16 text-white text-3xl"></span>
+                            <span className="fi fi-br-shield-check p-4 rounded-full bg-lime-500 size-16 text-white text-3xl"></span>
                             <div className="justify-center flex flex-col w-40 text-left">
-                                <h5 className="font-bold text-xl">Smart Watching</h5>
-                                <p className="w-50">AI-Powered Matching to find the perfect pet for you.</p>
+                                <h5 className="font-bold text-xl">Verified Providers</h5>
+                                <p className="w-50">All Providers are verified to ensure safety and trust.</p>
                             </div>
                         </div>
                         <div className="justify-center items-center flex w-90 text-left gap-3">
-                            <span className="fi fi-sr-sparkles p-4 rounded-full bg-purple-900 size-16 text-white text-3xl"></span>
+                            <span className="fi fi-sr-heart p-4 rounded-full bg-amber-400 size-16 text-white text-3xl"></span>
                             <div className="justify-center flex flex-col w-40 text-left">
-                                <h5 className="font-bold text-xl">Smart Watching</h5>
-                                <p className="w-50">AI-Powered Matching to find the perfect pet for you.</p>
+                                <h5 className="font-bold text-xl">Easy Adoption</h5>
+                                <p className="w-50">Simple steps from discovery to adoption.</p>
                             </div>
                         </div>
                         <div className="justify-center items-center flex w-90 text-left gap-3">
-                            <span className="fi fi-sr-sparkles p-4 rounded-full bg-purple-900 size-16 text-white text-3xl"></span>
+                            <span className="fi fi-sr-bubble-discussion p-4 rounded-full bg-blue-300 size-16 text-white text-3xl"></span>
                             <div className="justify-center flex flex-col w-40 text-left">
-                                <h5 className="font-bold text-xl">Smart Watching</h5>
-                                <p className="w-50">AI-Powered Matching to find the perfect pet for you.</p>
+                                <h5 className="font-bold text-xl">Ongoing Support</h5>
+                                <p className="w-50">We're here to support you every step to the way.</p>
                             </div>
                         </div>
                     </div>
